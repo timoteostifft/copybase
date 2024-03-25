20bd9230a827f52fc7d648e85a3995f4bb55ddc2
timoteostifft <timoteostifft@gmail.com>
2024-03-24 23:10:38 -0300

    feat: header

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,9 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Header from "./components/Header.vue";
+</script>
 
-<template></template>
+<template>
+  <Header />
+</template>
 
 <style scoped></style>
---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="container">
+    <div id="content">
+      <a href="">
+        <img src="../assets/logo.svg" alt="Copybase" />
+      </a>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+#container {
+  width: 100%;
+  background-color: var(--purple-900);
+}
+
+#content {
+  max-width: 1280px;
+  padding: 10px 20px;
+  margin: 0 auto;
+}
+
+#content > a > img {
+  width: 130px;
+}
+</style>
