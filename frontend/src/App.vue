9d41cf8df08f7f18d8a66f1ea44f22b03b215940
timoteostifft <timoteostifft@gmail.com>
2024-03-25 00:00:13 -0300

    feat: template

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
 import Header from "./components/Header.vue";
+import Template from "./components/Template.vue";
 </script>
 
 <template>
   <Header />
+  <Template />
 </template>
 
 <style scoped></style>
+./components/Template.vue
---
+++ frontend/src/components/Template.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="container">
+    <div id="content">
+      <h1>Explore as métricas do seu negócio</h1>
+      <p>
+        Descubra insights sobre seu negócio visualizando métricas chaves.
+        Carregue uma planilha de dados de assinantes e obtenha análises precisas
+        em gráficos interativos.
+      </p>
+      <button>BAIXAR MODELO</button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+#container {
+  width: 100%;
+}
+
+#content {
+  max-width: 1280px;
+  padding: 0 20px;
+  margin: 40px 20px 0 20px;
+  text-align: center;
+}
+
+#content > h1 {
+  text-align: center;
+  color: var(--purple-900);
+  font-weight: 700;
+  line-height: 2.25rem;
+}
+
+#content > p {
+  margin-top: 20px;
+  color: var(--text);
+}
+
+#content > button {
+  margin-top: 20px;
+  background-color: var(--green);
+  color: var(--background);
+  font-weight: 900;
+  padding: 10px 20px;
+  font-size: 1.25rem;
+}
+</style>
